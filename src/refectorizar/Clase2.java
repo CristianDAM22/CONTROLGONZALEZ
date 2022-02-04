b219e405a68b02cd12b0c8c1ee9f4d0669a7308f
dam <dam@203P01>
2022-02-04 11:48:46 +0100

    SEGUNDO COMMIT

---
+++ src/refectorizar/Clase2.java
@@ -0,0 +1,10 @@
+package refectorizar;
+
+public class Clase2 {
+
+    public Clase2(){
+        
+        System.out.println("ESTOY EN LA RAMA DE PRODUCCION");
+        System.out.println("ESPERO NO LIARLA");
+    }
+}
