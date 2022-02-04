97844f90982b50c9172d801b632de8023e631698
dam <dam@203P01>
2022-02-04 11:30:59 +0100

    PRIMER COMMIT

---
+++ src/refectorizar/Clase1.java
@@ -0,0 +1,12 @@
+
+package refectorizar;
+
+public class Clase1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Linea 1");
+        System.out.println("Hola mundos");
+    }
+    
+}
